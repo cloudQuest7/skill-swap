bdc78bb55cf67298f73eb1364a527f1af75a45df
shikha4-aa <shikhamandal2005@gmail.com>
2025-07-12 13:17:04 +0530

    Added full swap logic: request, view, accept, reject, delete

---
+++ skill_swap/backend/models/Swap.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const swapSchema = new mongoose.Schema({
+  fromUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  toUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  skillOffered: {
+    type: String,
+    required: true
+  },
+  skillRequested: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Swap', swapSchema);
